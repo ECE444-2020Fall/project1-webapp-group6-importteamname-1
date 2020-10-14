bbdcc9e763664af127654129af2ad333f77bd260
Tim Fei <tim.fei@mail.utoronto.ca>
2020-10-14 14:59:38 -0400

    Changed HTTP statuses from integers to HTTPStatus constants

--- server/constants.py
+++ server/constants.py
@@ -1,11 +1,12 @@
 import os
+from http import HTTPStatus
 
 CONSTANTS = {
     'PORT': os.environ.get('PORT', 3001),
     'HTTP_STATUS': {
-        '404_NOT_FOUND': 404,
-        '201_CREATED': 201,
-        '500_INTERNAL_SERVER_ERROR': 500
+        '404_NOT_FOUND': HTTPStatus.NOT_FOUND,
+        '201_CREATED': HTTPStatus.CREATED,
+        '500_INTERNAL_SERVER_ERROR': HTTPStatus.INTERNAL_SERVER_ERROR
     },
     'ENDPOINT': {
         'MASTER_DETAIL': '/api/masterdetail',
