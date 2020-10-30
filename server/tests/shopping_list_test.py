2c75a76a608156fdd904492b8abcc7e29d03b7fd
yanisakham <yanisagreen@gmail.com>
2020-10-30 18:38:33 -0400

    add name as comment to tests

--- server/tests/shopping_list_test.py
+++ server/tests/shopping_list_test.py
@@ -13,12 +13,14 @@ def set_client_user_id(client):
     with client.session_transaction() as sess:
         sess["user_id"] = uuid.uuid4()
 
+# yanisa's test
 def test_showing_shopping_list(client):
     """ make sure that shopping list returns 200 status code if user is logged in """
     set_client_user_id(client)
     rv = client.get('/api/shopping_list')
     assert(rv.status_code == 200)
 
+# yanisa's test
 def test_invalid_user_shopping_list(client):
     """ make sure that shopping list returns 500 status code if user is not logged in """
     rv = client.get('/api/shopping_list')
