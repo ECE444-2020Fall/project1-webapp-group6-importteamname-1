1e68b09a67cf24ee9c16a7880dad1a1ed47f085d
Mohamed Abdelhamid <abdelhamidm@MacBook-Pro-7.local>
2020-11-11 18:22:07 -0500

    Fixing merge conflicts

--- server/app/model_schemas/__init__.py
+++ server/app/model_schemas/__init__.py
@@ -7,8 +7,6 @@ from .shopping_list import ShoppingList
 from .user import User
 from .user_notes import UserNotes
 from .user_rating import UserRating
-<<<<<<< HEAD
 from .consumed_recipes import ConsumedRecipes
-=======
 from .user_favourites import UserFavourites
->>>>>>> edf9b0ce14ac11a4811e80c684e9ca96859af17c
+
