bcafd1fec4fec83f2bfe83e8b927172485885cbe
Tim Fei <tim.fei@mail.utoronto.ca>
2020-11-13 22:00:14 -0500

    Created card components for recipe search results

--- src/pages/RecipeSearchResults/RecipeSearchResults.jsx
+++ src/pages/RecipeSearchResults/RecipeSearchResults.jsx
@@ -4,6 +4,17 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux'; 
 import { getRecipes } from '../../actions/recipeActions';
 import PropTypes from 'prop-types';
+import { Grid, Paper, Typography } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import { UserRating } from "../../components/UserRating";
+import { FavouritesButton } from "../../components/FavouritesButton";
+import { RecipeCartButton } from "../../components/RecipeCartButton";
+import foodImg from "./food.png"; // Placeholder food image
 
 const RecipeSearchResults = (props) => {
   useEffect(() => {
@@ -34,12 +45,52 @@ const RecipeSearchResults = (props) => {
   }
 
   return (
-    <main id="mainContent" className="container">
-      <div className="row justify-content-center py-5">
-        <h3>Recipe Search Results</h3>
-      </div>
-         {recipeSearchResult}
-    </main>
+    // <main id="mainContent" className="container">
+    //   <div className="row justify-content-center py-5">
+    //     <h3>Recipe Search Results</h3>
+    //   </div>
+    //      {recipeSearchResult}
+    // </main>
+
+    <div style={{ marginTop: 20, padding: 130 }}>
+      <Grid container spacing={60} justify="center">
+        {props.data.recipes.map(recipe => (
+          <Link key={recipe.recipe_id} to={`recipe-search-results/${recipe.recipe_id}`}>
+            <Grid item key={recipe.recipe_id}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="280"
+                    image={foodImg}
+                    title="Contemplative Reptile"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {recipe.recipe_name} 
+                    </Typography>
+                    <Typography component="p">
+                      {recipe.calories} Calories
+                    </Typography>
+                    <br></br>
+                    <Typography component="p">
+                      <UserRating recipe_id={recipe.recipe_id} />
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <RecipeCartButton recipe_id={recipe.recipe_id} />
+                  <FavouritesButton recipe_id={recipe.recipe_id} />
+                </CardActions>
+              </Card>
+            </Grid>
+          </Link>
+        ))}
+      </Grid>
+    </div>
+
+
   );
 }
 
