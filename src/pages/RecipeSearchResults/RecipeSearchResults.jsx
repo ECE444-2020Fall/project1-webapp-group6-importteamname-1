cfaa6ac6ef011e8370c4f34b9f99a1b16f8fc0a2
Tim Fei <tim.fei@mail.utoronto.ca>
2020-11-14 00:47:26 -0500

    Added pagination for recipe search results

--- src/pages/RecipeSearchResults/RecipeSearchResults.jsx
+++ src/pages/RecipeSearchResults/RecipeSearchResults.jsx
@@ -12,25 +12,43 @@ import CardMedia from "@material-ui/core/CardMedia";
 import { UserRating } from "../../components/UserRating";
 import { FavouritesButton } from "../../components/FavouritesButton";
 import { RecipeCartButton } from "../../components/RecipeCartButton";
-import foodImg from "./food.png"; // Placeholder food image
 import { makeStyles } from "@material-ui/core/styles";
+import Pagination from "@material-ui/lab/Pagination";
+import { List, ListItem, Divider, Box } from "@material-ui/core";
+import foodImg from "./food.png"; // Placeholder food image
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     padding: theme.spacing(2),
     marginTop: 100,
-    marginLeft: 150,
+    marginLeft: 100,
+    width: "100%"
+  },
+  paginator: {
+    justifyContent: "center",
+    padding: "10px"
   }
 }));
 
 const RecipeSearchResults = (props) => {
+  const classes = useStyles();
+  let recipeSearchResult = null;
+  const itemsPerPage = 5;
+  const [page, setPage] = React.useState(1);
+  const [noOfPages] = React.useState(
+    Math.ceil(props.data.recipes.length / itemsPerPage)
+  );
+
+  console.log(props.data.recipes.length);
+
   useEffect(() => {
     props.getRecipes()
   }, []);
 
-  const classes = useStyles();
-  let recipeSearchResult = null;
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
 
   if (props.data.recipes) { 
     recipeSearchResult =  <div className={classes.root}>
@@ -41,7 +59,8 @@ const RecipeSearchResults = (props) => {
             justify="flex-start"
             alignItems="flex-start"
           >
-          {props.data.recipes.map(recipe => (
+          {props.data.recipes.slice((page - 1) * itemsPerPage, page * itemsPerPage)
+          .map(recipe => (
             <Grid item key={recipe.recipe_id} xs={25}>
               <Card>
                 <CardActionArea>
@@ -76,6 +95,21 @@ const RecipeSearchResults = (props) => {
             </Grid>
           ))}
         </Grid>
+        <br></br>
+        <Divider />
+        <Box component="span">
+          <Pagination
+            count={noOfPages}
+            page={page}
+            onChange={handleChange}
+            defaultPage={1}
+            color="primary"
+            size="large"
+            showFirstButton
+            showLastButton
+            classes={{ ul: classes.paginator }}
+          />
+        </Box>
       </div>
   } else {
     recipeSearchResult = <p>No recipes available.</p>
