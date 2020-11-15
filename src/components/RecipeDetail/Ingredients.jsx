f8f74d56c28dc1f3bc39550175b2064bea1c451e
Tim Fei <tim.fei@mail.utoronto.ca>
2020-11-14 23:20:05 -0500

    Added styling for ingredients and nutrition facts panels

--- src/components/RecipeDetail/Ingredients.jsx
+++ src/components/RecipeDetail/Ingredients.jsx
@@ -1,28 +1,90 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import EcoIcon from '@material-ui/icons/Eco';
+import RestaurantIcon from '@material-ui/icons/Restaurant';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 752,
+  },
+  demo: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  title: {
+    margin: theme.spacing(4, 0, 2),
+  },
+}));
 
 const Ingredients = (props) => {
+  const classes = useStyles();
+  let recipeServings = null;
   let recipeIngredients = null;
 
   if (props.ingredientsList.ingredients) { 
+    recipeServings = 
+      <Grid container>
+        <Grid item xs={12} md={6}>
+          <Typography fontWeight={500} variant="h7" className={classes.title}>
+            Servings:
+          </Typography>
+          <div className={classes.demo}>
+            <List dense={true}>              
+              <ListItem>
+                <ListItemIcon>
+                  <RestaurantIcon />
+                </ListItemIcon>
+                <ListItemText
+                  primary={(props.servings) + (props.servings > 1 ? 'meals' : 'meal')}
+                />
+              </ListItem>
+            </List>
+          </div>
+        </Grid>
+      </Grid>
+
     recipeIngredients = 
-      <div>
-        {props.ingredientsList.ingredients.map(ingredient => (
-          <p key={`${ingredient.recipe_id}${ingredient.ingredient_name}`}>
-          {ingredient.ingredient_name} {ingredient.amount} {ingredient.unit_of_measurement}</p>
-        ))}
-      </div>
+      <Grid container>
+        <Grid item xs={12} md={6}>
+          <Typography fontWeight={500} variant="h7" className={classes.title}>
+            Ingredients:
+          </Typography>
+          <div className={classes.demo}>
+            <List dense={true}>              
+        
+            {props.ingredientsList.ingredients.map(ingredient => (
+              <ListItem>
+                <ListItemIcon>
+                  <EcoIcon />
+                </ListItemIcon>
+                <ListItemText
+                  primary={ingredient.ingredient_name}
+                  secondary={'(' + ingredient.amount + ' ' + ingredient.unit_of_measurement + ')'}
+                />
+              </ListItem>
+            ))}
+            </List>
+          </div>
+        </Grid>
+      </Grid>
   } else {
     recipeIngredients = <p>No Ingredients.</p>
   }
   
   return (
-      <div>
-        Servings: 
-        {props.servings}
-        Ingredients:
+    <div>
+        {recipeServings}
         {recipeIngredients}
-      </div>
+    </div>
     );
   }
   
--- src/components/RecipeDetail/NutritionFacts.jsx
+++ src/components/RecipeDetail/NutritionFacts.jsx
@@ -1,17 +1,86 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import BatteryChargingFullIcon from '@material-ui/icons/BatteryChargingFull';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 752,
+  },
+  demo: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  title: {
+    margin: theme.spacing(4, 0, 2),
+  },
+}));
+
 
 const NutritionFacts = (props) => {
-    return (
-      <div>
-        Nutrition Facts:
-        <p>Calories: {props.calories} </p>
-        <p>Protein: {props.protein} </p>
-        <p>Carbs: {props.carbs}</p>
-        <p>Fat: {props.fat}</p>
-      </div>
-    );
-  }
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Grid container>
+        <Grid item xs={12} md={6}>
+          <Typography fontWeight={500} variant="h7" className={classes.title}>
+            Nutrition Facts:
+          </Typography>
+          <div className={classes.demo}>
+            <List dense={true}>
+                <ListItem>
+                  <ListItemIcon>
+                    <BatteryChargingFullIcon />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={props.calories}
+                    secondary='Calories'
+                  />
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon>
+                    <BatteryChargingFullIcon />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={props.carbs + ' g'}
+                    secondary='Carbs'
+                  />
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon>
+                    <BatteryChargingFullIcon />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={props.protein + ' g'}
+                    secondary='Protein'
+                  />
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon>
+                    <BatteryChargingFullIcon />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={props.fat + ' g'}
+                    secondary='Fat'
+                  />
+                </ListItem>
+            </List>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
   
 NutritionFacts.propTypes = {
   calories: PropTypes.number,
@@ -20,4 +89,4 @@ NutritionFacts.propTypes = {
   fat: PropTypes.number,
 };
 
-  export default NutritionFacts;
+export default NutritionFacts;
