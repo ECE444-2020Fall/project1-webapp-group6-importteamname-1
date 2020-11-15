cd03c63348792572f1dbfaf049afb7b2d3008403
Tim Fei <tim.fei@mail.utoronto.ca>
2020-11-14 19:40:28 -0500

    De-coupled RecipeCard styling

--- src/pages/RecipeSearchResults/RecipeSearchResults.jsx
+++ src/pages/RecipeSearchResults/RecipeSearchResults.jsx
@@ -2,19 +2,11 @@
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux'; 
 import PropTypes from 'prop-types';
-import { Grid, Typography } from "@material-ui/core";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import { UserRating } from "../../components/UserRating";
-import { FavouritesButton } from "../../components/FavouritesButton";
-import { RecipeCartButton } from "../../components/RecipeCartButton";
+import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Pagination from "@material-ui/lab/Pagination";
 import { Divider, Box } from "@material-ui/core";
-import foodImg from "./food.png"; // Placeholder food image
+import RecipeCard from "../../components/common/RecipeCard";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -39,8 +31,6 @@ const RecipeSearchResults = (props) => {
     Math.ceil(props.data.recipes.length / itemsPerPage)
   );
 
-  console.log(props.data.recipes.length);
-
   useEffect(() => {
     props.clearRecipeSortFilter();
   }, []);
@@ -59,7 +49,8 @@ const RecipeSearchResults = (props) => {
     if (currentSortOrder === "descending") {
       sortedRecipes = recipesToBeSorted.sort((recipeA, recipeB) => recipeA[valueToBeSorted] - recipeB[valueToBeSorted]);
       props.sortRecipesAscending(sortedRecipes, valueToBeSorted.toUpperCase());
-    } else { // currentSortOrder is initialized to ''. When the user clicks the sort button for the first time, we sort recipes descending by default.
+    } else { 
+      // currentSortOrder is initialized to ''. When the user clicks the sort button for the first time, we sort recipes descending by default.
       sortedRecipes = recipesToBeSorted.sort((recipeA, recipeB) => recipeB[valueToBeSorted] - recipeA[valueToBeSorted]);
       props.sortRecipesDescending(sortedRecipes, valueToBeSorted.toUpperCase());
     } 
@@ -79,38 +70,23 @@ const RecipeSearchResults = (props) => {
           {props.data.recipes
           .slice((page - 1) * itemsPerPage, page * itemsPerPage)
           .map(recipe => (
-            <Grid item key={recipe.recipe_id} xs={25}>
-              <Card>
-                <CardActionArea>
-                  <Link key={recipe.recipe_id} to={`recipe-search-results/${recipe.recipe_id}`} >
-                    <CardMedia
-                      component="img"
-                      alt="imageUnavailable"
-                      height="280"
-                      image={foodImg}
-                    />
-                  </Link>
-                  <CardContent>
-                    <Link key={recipe.recipe_id} to={`recipe-search-results/${recipe.recipe_id}`} >
-                      <Typography color="textPrimary" gutterBottom variant="h5" component="h2">
-                        {recipe.recipe_name} 
-                      </Typography>
-                      <Typography color="textPrimary" component="p">
-                        {recipe.calories} Calories
-                      </Typography>
-                    </Link>
-                    <br></br>
-                    <Typography >
-                      <UserRating recipe_id={recipe.recipe_id} />
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>     
-                <CardActions>
-                  <RecipeCartButton recipe_id={recipe.recipe_id} />
-                  <FavouritesButton recipe_id={recipe.recipe_id} />
-                </CardActions>
-              </Card>
+          <Grid item key={props.recipe_id} xs={25}>
+              <Link key={recipe.recipe_id} to={`recipe-search-results/${recipe.recipe_id}`}>
+                <RecipeCard 
+                  key={recipe.recipe_id}
+                  recipeId={recipe.recipe_id} 
+                  recipeName={recipe.recipe_name}
+                  imageUrl={recipe.image_url}
+                  timeToCookInMinutes={recipe.time_to_cook_in_minutes}
+                  servings={recipe.servings}
+                  calories={recipe.calories}
+                  protein={recipe.protein}
+                  carbs={recipe.carbs}
+                  fat={recipe.fat} 
+                />
+              </Link>
             </Grid>
+
           ))}
         </Grid>
         <br></br>
@@ -147,6 +123,7 @@ const RecipeSearchResults = (props) => {
 
 RecipeSearchResults.propTypes = {
   data: PropTypes.object,
+  recipe_id: PropTypes.string,
   getRecipes: PropTypes.object,
   sortRecipesAscending: PropTypes.func,
   sortRecipesDescending: PropTypes.func,
