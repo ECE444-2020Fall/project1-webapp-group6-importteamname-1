354acd1f8691517948d546b55f848c833f114efc
yanisakham <yanisagreen@gmail.com>
2020-11-17 15:41:59 -0500

    remove unused endpoints and commented out code

--- server/app/routes/get_routes.py
+++ server/app/routes/get_routes.py
@@ -5,6 +5,7 @@ from constants import CONSTANTS
 from . import inventory_manager, ingredient_controller, recipe_controller
 from utils.helper_functions import * 
 
+
 @app.route('/api/<any(user_notes,user_rating,recipe_cart,favourites_list):model>/<string:recipe_id>')
 def get_item_from_model(model, recipe_id):
     user_id = get_user_id()
@@ -12,6 +13,7 @@ def get_item_from_model(model, recipe_id):
         return user_id_not_found_response() 
     model = models_map[model]
     return inventory_manager.get_item(user_id, recipe_id, model)
+
 
 @app.route('/api/<any(user_notes,user_rating,shopping_list,pantry_list):model>')
 def get_all_user_items_from_model(model):
@@ -21,6 +23,7 @@ def get_all_user_items_from_model(model):
     model = models_map[model]
     return inventory_manager.get_all_user_items(user_id, model)
 
+
 @app.route('/api/<any(recipe_cart, favourites_list):model>')
 def get_all_user_recipes_from_model(model):
     user_id = get_user_id()
@@ -29,6 +32,7 @@ def get_all_user_recipes_from_model(model):
     model = models_map[model]
     user_carted_recipe_ids = inventory_manager.get_all_user_items(user_id, model).get_json()["items"]
     return recipe_controller.get_recipes_by_ids(user_carted_recipe_ids, Recipe)
+
 
 @app.route('/api/smart_shopping_list')
 def generate_smart_shopping_list_items():
@@ -41,34 +45,23 @@ def generate_smart_shopping_list_items():
     recipe_ingredients = ingredient_controller.get_ingredient_names_by_recipe_ids(RecipeIngredient, carted_recipe_ids).get_json()["ingredients"]
     smart_ingredients = list(set(recipe_ingredients) - set(pantry_list_items) - set(shopping_list_items))
     
-    response = {"items": smart_ingredients}
-    return make_response(jsonify(response), CONSTANTS['HTTP_STATUS']['200_OK'])
+    json_response = jsonify({"items": smart_ingredients})
+    return make_response(json_response, CONSTANTS['HTTP_STATUS']['200_OK'])
+
 
 @app.route('/api/ingredients/<string:recipe_id>')
 def get_ingredient_by_recipe_id(recipe_id):
     return ingredient_controller.get_ingredient_by_recipe_id(RecipeIngredient, recipe_id)
 
+
 @app.route('/api/recipes')
 def get_all_recipes():
     return recipe_controller.get_all_recipes(Recipe)
 
-@app.route('/api/get_user')
-def get_user():
-    user_id = get_user()
-    if not user_id:
-        return user_id_not_found_response() 
-
-    user = User.query.filter(User.user_id == user_id).first()    
-    if not user:
-        json_response = jsonify({'error': 'sessions user not in database'})
-        return make_response(json_response, CONSTANTS['HTTP_STATUS']['500_INTERNAL_SERVER_ERROR'])
-
-    json_response = jsonify({'username': user.username})
-    return make_response(json_response, CONSTANTS['HTTP_STATUS']['200_OK'])
 
 @app.route('/api/logout')
 def logout_user():
-    if 'user_id' in session:
-        del session['user_id']
-        return make_response(CONSTANTS['HTTP_STATUS']['200_OK'])
-    return make_response(CONSTANTS['HTTP_STATUS']['400_BAD_REQUEST'])
+    if 'user_id' not in session:
+        return user_id_not_found_response() 
+    del session['user_id']
+    return make_response(CONSTANTS['HTTP_STATUS']['200_OK'])
