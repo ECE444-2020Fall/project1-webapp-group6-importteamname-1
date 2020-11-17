a432dad6551292bf91f3cd3a9e65a808cf843630
Tim Fei <tim.fei@mail.utoronto.ca>
2020-11-17 14:47:37 -0500

    Unit test for POST recipe working

--- server/tests/ingredients_test.py
+++ server/tests/ingredients_test.py
@@ -3,14 +3,21 @@ import pytest
 from server import app
 import uuid
 
-
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
     return app.test_client()
 
 def test_get_all_ingredients(client): 
-    result = client.get('/api/ingredients')
-    assert(result.status_code == 200)
-    
+    response = client.get('/api/ingredients')
+    assert(response.status_code == 200)
+    assert(response.content_type == "application/json")
 
+def test_add_ingredient(client):
+    pass
+    # response = client.get('/api/ingredients/add')
+    # assert(response.status_code == 200)
+    # assert(response.content_type == "application/json")
+
+def test_get_ingredient_by_recipe_id(client):
+    pass
--- server/tests/recipes_test.py
+++ server/tests/recipes_test.py
@@ -1,5 +1,7 @@
 import pytest
-
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import json
 from server import app
 import uuid
 
@@ -10,7 +12,54 @@ def client():
     return app.test_client()
 
 def test_get_all_recipes(client): 
-    result = client.get('/api/recipes')
-    assert(result.status_code == 200)
+    """Test that get all recipes work"""
+
+    # Act
+    response = client.get('/api/recipes')
     
+    # Assert
+    assert(response.status_code == 200)
+    assert(response.content_type == 'application/json')
+    assert('recipes' in response.json)
+    assert("recipe_id" in response.json["recipes"][0])
+    assert("recipe_name" in response.json["recipes"][0])
+    assert("image_url" in response.json["recipes"][0])
+    assert("cuisine" in response.json["recipes"][0])
+    assert("instructions" in response.json["recipes"][0])
+    assert("time_to_cook_in_minutes" in response.json["recipes"][0])
+    assert("servings" in response.json["recipes"][0])
+    assert("calories" in response.json["recipes"][0])
+    assert("protein" in response.json["recipes"][0])
+    assert("carbs" in response.json["recipes"][0])
+    assert("fat" in response.json["recipes"][0])
+
+def test_add_recipe(client):
+    """Test that adding a single recipe works"""
+
+    # Arrange
+    request_header = {'Content-Type': 'application/json'}
+    request_body = {
+        "recipe_id": "00000000-0000-0000-0000-000000000000",
+        "recipe_name": "name",
+        "image_url": "image_url",
+        "cuisines": "cuisine1, cuisine2, cuisine3",
+        "instructions": "steps",
+        "time_to_cook_in_minutes": 2,
+        "servings": 2,
+        "calories": 2,
+        "protein": 2,
+        "carbs": 2,
+        "fat": 2
+    }
+
+    # Act
+    response = client.post('/api/recipes/add', data=json.dumps(request_body), headers=request_header)
+    # responseData = str(response).lower()
+
+    # Assert
+    assert(response.status_code == 201)
+    assert(response.content_type == 'application/json')
+    
+
+
 
