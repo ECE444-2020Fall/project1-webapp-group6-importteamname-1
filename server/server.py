a81f2f83c3fb67f17a97901348990a27949545c7
yanisakham <yanisagreen@gmail.com>
2020-11-18 09:05:13 -0500

    cors

--- server/server.py
+++ server/server.py
@@ -23,6 +23,13 @@ def page_not_found(error):
     json_response = jsonify({'error': 'Page not found'})
     return make_response(json_response, CONSTANTS['HTTP_STATUS']['404_NOT_FOUND'])
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,POST,DELETE')
+  return response
+
 if __name__ == '__main__':
     app.run(port=CONSTANTS['PORT'])
     db.create_all()
