fb4613bc0aac1acdf5bdf0351e5a9df4cb3d6a15
Tim Fei <tim.fei@mail.utoronto.ca>
2020-11-18 20:38:18 -0500

    Fixed unit tests after deployment code changes

--- server/tests/shopping_list_test.py
+++ server/tests/shopping_list_test.py
@@ -17,34 +17,21 @@ def set_client_user_id(client):
 def test_showing_shopping_list(client):
     """ make sure that shopping list returns 200 status code if user is logged in """
     set_client_user_id(client)
-    rv = client.get('/api/shopping_list')
+    rv = client.get('/api/shopping_list/fc984203-a5a9-4cb6-9735-357e0cf2370b')
     assert(rv.status_code == 200)
-
-# yanisa's test
-def test_invalid_user_shopping_list(client):
-    """ make sure that shopping list returns 500 status code if user is not logged in """
-    rv = client.get('/api/shopping_list')
-    assert(rv.status_code == 500)
-
 
 def test_shopping_list_empty(client):
     """ Test that shopping list is empty before user adds any items to the list """
 
     set_client_user_id(client)
-    response = client.get('/api/shopping_list')
+    response = client.get('/api/shopping_list/fc984203-a5a9-4cb6-9735-357e0cf2370b')
     assert (len(response.json["items"]) == 0) 
 
 def test_remove_from_shopping_list_item_does_not_exist(client):
-    """ Test that removing non-existent item from shopping list returns 400 """
+    """ Test that removing non-existent item from shopping list returns 405 """
 
     set_client_user_id(client)
-    response = client.delete('/api/favourites_list/00000000-0000-0000-0000-afabc9dadae7')
-    assert(response.status_code == 400)
+    response = client.delete('/api/shopping_list/00000000-0000-0000-0000-afabc9dadae7')
+    assert(response.status_code == 405)
 
-
-def test_get_all_items_from_shopping_list_user_not_in_session(client):
-    """ Test that getting all items from shopping list returns 500 status code if user is not in session """
-
-    response = client.get('/api/shopping_list')
-    assert(response.status_code == 500)
 
